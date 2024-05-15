af4aa9d8b65ee01486666f7bfca87d784a342f85
Briantam0422 <briantam0422@gmail.com>
2024-05-14 23:49:40 -0700

    setup nextui

---
+++ src/providers/Providers.tsx
@@ -0,0 +1,10 @@
+import { NextUIProvider } from "@nextui-org/react";
+import React from "react";
+
+type ProvidersProps = {
+  children: React.ReactNode;
+};
+
+export function Providers(props: ProvidersProps) {
+  return <></>;
+}
