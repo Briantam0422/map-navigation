34d6fb6169387c9f68cd6122a980a86943313e9c
Briantam0422 <briantam0422@gmail.com>
2024-05-17 13:50:45 -0700

    add mock api

---
+++ src/api/mock/mockRoute.ts
@@ -0,0 +1,38 @@
+import { getRequest, postRequest } from "@/util/api";
+import { DataGetRouteProps, DataPostRouteProps } from '../route';
+
+export async function postMockRouteError() {
+    const url = "/mock/route/500";
+    const data: DataPostRouteProps = await postRequest({url});
+    return data;
+}
+
+export async function postMockRouteSuccess() {
+    const url = "/mock/route/success";
+    const data: DataPostRouteProps = await postRequest({url});
+    return data;
+}
+
+export async function getMockRouteError() {
+    const url = "/mock/route/500";
+    const data: DataGetRouteProps = await getRequest({url});
+    return data;
+}
+
+export async function getMockRouteInprogress(){
+    const url = "/mock/route/inprogress";
+    const data: DataGetRouteProps = await getRequest({url});
+    return data;
+}
+
+export async function getMockRouteFailure(){
+    const url = "/mock/route/failure";
+    const data: DataGetRouteProps = await getRequest({url});
+    return data;
+}
+
+export async function getMockRouteSuccess(){
+    const url = "/mock/route/success";
+    const data: DataGetRouteProps = await getRequest({url});
+    return data;
+}
--- src/api/route.ts
+++ src/api/route.ts
@@ -18,12 +18,12 @@ export type DataPostRouteProps = {
     token: string
 }
 export async function getRoute({token}: ReqGetRouteProps){
-    const url = "/mock/route/success"
+    const url = `/route/${token}`
     const data: DataGetRouteProps = await getRequest({url})
     return data
 }
 export async function postRoute(body: ReqPostRouteProps){
-    const url = "/mock/route/success"
+    const url = `/route`
     const data:DataPostRouteProps = await postRequest({url, body})
     return data
 }
