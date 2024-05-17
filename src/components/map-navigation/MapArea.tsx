03b0bde2d95e85e70942fa143b4717529782bf22
Briantam0422 <briantam0422@gmail.com>
2024-05-17 13:52:18 -0700

    avoid using any type

--- src/components/map-navigation/MapArea.tsx
+++ src/components/map-navigation/MapArea.tsx
@@ -1,6 +1,6 @@
 import { config } from "@/config/app";
 import { Spinner } from "@nextui-org/react";
-import { useLoadScript } from "@react-google-maps/api";
+import { Libraries, useLoadScript } from "@react-google-maps/api";
 import { useMemo } from "react";
 import RenderMap from "./RenderMap";
 
@@ -8,7 +8,7 @@ export default function MapArea() {
   const libraries = useMemo(() => ["places"], []);
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: config.google_map_key as string,
-    libraries: libraries as any,
+    libraries: libraries as Libraries | undefined,
   });
   return isLoaded ? <RenderMap /> : <Spinner />;
 }
